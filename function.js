b6141fd37ea9a552f46d41ba0dbd4759ecf4f321
mdshakil04 <m.shakil04@yahoo.com>
2023-12-27 23:44:29 +0600

    Execution Context practice

---
+++ function.js
@@ -0,0 +1,40 @@
+
+// const button = document.getElementById("btn-click")
+
+// function clickFunction(){
+//     alert("Do not touch me")
+//  }
+
+// button.addEventListener("click", clickFunction)
+
+// let x = 5;
+// let y = 6;
+// x = y;
+// y = 8;
+// console.log(x)
+// console.log(y)
+// const skills = ["JS", "Python", "REACT"]
+
+// skills.push("Firebase")
+// console.log(skills)
+
+// Array Methods
+// const fruits = ["Banana", "Mango", "Orange", "Apple", "Pineapple"]
+
+// const result1 = fruits.find((f) => f === "Orange");
+// const result2 = fruits.filter((f) => f === "Orange" || f === "Apple");
+// const result3 = fruits.slice(1, 4);
+// console.log(result1)
+// console.log(result2)
+// console.log(result3)
+var n = 5;
+var p = 12;
+function square(n, p){
+    var value = n * p;
+    return value
+}
+
+var square1 = square(n ,p)
+var square2 = square(12, 14)
+console.log(square1)
+console.log(square2)
