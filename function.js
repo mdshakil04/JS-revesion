aa9d4e61eace472230c56b4302421aaca97d178f
mdshakil04 <m.shakil04@yahoo.com>
2023-12-31 15:47:31 +0600

    If-Else condition practice

--- function.js
+++ function.js
@@ -7,26 +7,26 @@
 
 // button.addEventListener("click", clickFunction)
 
-// let x = 5;
-// let y = 6;
-// x = y;
-// y = 8;
-// console.log(x)
-// console.log(y)
-// const skills = ["JS", "Python", "REACT"]
+let x = 5;
+let y = 6;
+x = y;
+y = 8;
+console.log(x)
+console.log(y)
+const skills = ["JS", "Python", "REACT"]
 
-// skills.push("Firebase")
-// console.log(skills)
+skills.push("Firebase")
+console.log(skills)
 
 // Array Methods
-// const fruits = ["Banana", "Mango", "Orange", "Apple", "Pineapple"]
+const fruits = ["Banana", "Mango", "Orange", "Apple", "Pineapple"]
 
-// const result1 = fruits.find((f) => f === "Orange");
-// const result2 = fruits.filter((f) => f === "Orange" || f === "Apple");
-// const result3 = fruits.slice(1, 4);
-// console.log(result1)
-// console.log(result2)
-// console.log(result3)
+const result1 = fruits.find((f) => f === "Orange");
+const result2 = fruits.filter((f) => f === "Orange" || f === "Apple");
+const result3 = fruits.slice(1, 4);
+console.log(result1)
+console.log(result2)
+console.log(result3)
 
 var n = 5;
 var p = 12;
@@ -41,10 +41,15 @@ console.log(square1)
 console.log(square2)
 
 /*
-1. Function can be assigned to a variable
-2.Function can be passed as an argument
-3. Function can be returned from another function
-4. Function can be stored in data structure like array, object etc.
-
-
+    1. Function can be assigned to a variable
+    2.Function can be passed as an argument
+    3. Function can be returned from another function
+    4. Function can be stored in data structure like array, object etc.
 */
+let now = new Date();
+let dayOfWeek = now.getDate();
+if(dayOfWeek == 31){
+    console.log("Have a nice weekend!")
+}else{
+    console.log("Wait for Weekend")
+}
