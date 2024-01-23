6495fde945596fd438fb8f93fcadeed8045f1b25
mdshakil04 <m.shakil04@yahoo.com>
2024-01-23 22:04:29 +0600

    ParseInt Practice

--- shorthand.js
+++ shorthand.js
@@ -2,4 +2,9 @@ var tomatoPrice = 25;
 // tomatoPrice = 30;
 
 var newTomatoPrice = tomatoPrice + 10;
-console.log(newTomatoPrice)
+// console.log(newTomatoPrice)
+var price1 = "12";
+var price2 = 10;
+var realPrice = parseInt(price1)
+var totalPrice = realPrice + price2;
+console.log(totalPrice)
