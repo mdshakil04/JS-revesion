c8ad3fbc738302f932fd295538a50bfa40d80a4a
mdshakil04 <m.shakil04@yahoo.com>
2024-01-24 20:56:12 +0600

    Started today

--- shorthand.js
+++ shorthand.js
@@ -3,8 +3,20 @@ var tomatoPrice = 25;
 
 var newTomatoPrice = tomatoPrice + 10;
 // console.log(newTomatoPrice)
-var price1 = "12";
-var price2 = 10;
+var price1 = "12"; var price2 = 10;
 var realPrice = parseInt(price1)
 var totalPrice = realPrice + price2;
 console.log(totalPrice)
+// -----------------
+
+/*
+var = Declares a variable
+let = Declares a block variable
+const = Declares a block constant
+if = Marks a block of statements to be executed on a condition
+switch = Marks a block of statements to be executed in different cases
+for = Marks a block of statements to be executed in a loop
+function = Declares a function
+return = Exits a function
+try = Implements error handling to a block of statements
+*/
