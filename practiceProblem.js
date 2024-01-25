2e65ac35a7786db28732c20d027ef3d611b6732f
mdshakil04 <m.shakil04@yahoo.com>
2024-01-25 19:01:09 +0600

    Practice Problem Started

---
+++ practiceProblem.js
@@ -0,0 +1,6 @@
+let totalMoney = 1000;
+let applePrice = 300;
+let mangoPrice = 400;
+let totalCost = applePrice + mangoPrice;
+let remainingMoney = totalMoney - totalCost;
+console.log(remainingMoney)
