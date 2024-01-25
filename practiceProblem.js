5f64371ad7d3b49e43964702ad571d26e2d5857a
mdshakil04 <m.shakil04@yahoo.com>
2024-01-25 19:23:30 +0600

    Average

--- practiceProblem.js
+++ practiceProblem.js
@@ -4,3 +4,14 @@ let mangoPrice = 400;
 let totalCost = applePrice + mangoPrice;
 let remainingMoney = totalMoney - totalCost;
 console.log(remainingMoney)
+
+// ---------------------------------
+let bangla = 75.25;
+let math = 65;
+let chemistry = 80;
+let biology = 35.45;
+let physics = 99.50;
+let totalMarks = bangla + math + chemistry + biology + physics;
+let average = totalMarks / 5;
+
+console.log(averageFloat)
