b316bc32d26d857fa4385875de5826aa6d1544ba
mdshakil04 <m.shakil04@yahoo.com>
2024-01-26 19:23:22 +0600

    All about array

---
+++ array.js
@@ -0,0 +1,7 @@
+var marks = [20, 30, 45, 32, 12, 24, 67]
+// marks.push(40)
+var removedLast = marks.pop()
+var removedFirst = marks.shift()
+console.log(removedLast)
+console.log(removedFirst)
+console.log(marks)
