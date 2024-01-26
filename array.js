2ca44a7143b09403497076b841cd5e2ae71c38d9
mdshakil04 <m.shakil04@yahoo.com>
2024-01-26 19:37:01 +0600

    Comparison Oparator

--- array.js
+++ array.js
@@ -1,5 +1,4 @@
 var marks = [20, 30, 45, 32, 12, 24, 67]
-// marks.push(40)
 var removedLast = marks.pop()
 var removedFirst = marks.shift()
 console.log(removedLast)
---
+++ comparison.js
@@ -0,0 +1,6 @@
+var num1 = 24;
+var num2 = 25;
+var num3 = 26;
+console.log(num1 !== num2)
+console.log(num1 <= num2)
+console.log(num1 < num2)
