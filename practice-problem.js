fd54f763278141a39ce437d62f0f99f0b08b6f83
mdshakil04 <m.shakil04@yahoo.com>
2024-01-27 16:14:58 +0600

    Question added

--- practice-problem.js
+++ practice-problem.js
@@ -1,8 +1,13 @@
 // JS Problem solving ...Practice problem 1
+/**
+ * Find the index of 'Banana' and replace "banana" with "Mango".
+ * Remove "Orange" and add "Watermelon"
+ */
 var fruits = ["Apple", "Banana", "Orange"];
 var fruitIndex = fruits.indexOf("Banana")
-// fruits[1] = "Mango"
+fruits[1] = "Mango"
 fruits.pop()
 fruits.push("Watermelon")
 console.log(fruits)
 console.log(fruitIndex)
+// JS Problem solving ...Practice problem 2
