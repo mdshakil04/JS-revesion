0f452ff8c5bb64b56ea076a6988dae7dc4a25a03
mdshakil04 <m.shakil04@yahoo.com>
2024-01-27 20:16:18 +0600

    Another Problem solved

--- practice-problem.js
+++ practice-problem.js
@@ -48,3 +48,17 @@ if (tomScore >= 80) {
 } else {
     console.log("Tom failed");
 }
+
+/**
+ * You are given three numbers 13, 79, and 45. Write a js program that will print the largest number using if-else.
+ */
+var number1 = 13;
+var number2 = 79;
+var number3 = 45;
+if(number1 > number2 && number1 > number3){
+    console.log("The largest number is " + number1)
+}else if(number2 > number1 && number2 > number3){
+    console.log("Largest number is " + number2)
+}else{
+    console.log("Largest number is " + number3)
+}
