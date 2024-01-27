b5bb672a69e4a15776b09549e7bb60f51fe323ec
mdshakil04 <m.shakil04@yahoo.com>
2024-01-27 16:12:47 +0600

    JS Problem solving

---
+++ practice-problem.js
@@ -0,0 +1,8 @@
+// JS Problem solving ...Practice problem 1
+var fruits = ["Apple", "Banana", "Orange"];
+var fruitIndex = fruits.indexOf("Banana")
+// fruits[1] = "Mango"
+fruits.pop()
+fruits.push("Watermelon")
+console.log(fruits)
+console.log(fruitIndex)
