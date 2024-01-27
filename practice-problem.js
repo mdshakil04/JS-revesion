e8e9a153f7c956dc3ec8534d69c86f7d24d3332a
mdshakil04 <m.shakil04@yahoo.com>
2024-01-27 16:20:09 +0600

    New Problem added

--- practice-problem.js
+++ practice-problem.js
@@ -11,3 +11,11 @@ fruits.push("Watermelon")
 console.log(fruits)
 console.log(fruitIndex)
 // JS Problem solving ...Practice problem 2
+/**
+ * You and your friends Tim, Jane, Peter and John got their final exam results. Your total score is 85, Tom's total is 66, Jane's total is 95, Peter's total is 56 and John's total is 40. The grading chatrs is
+ * 80 or above A grade
+ * 60 or above B grade
+ * 50 or above C grade
+ * 40 or above D grade
+ * Write a program to find your and your friends grades using if-else.
+ */
