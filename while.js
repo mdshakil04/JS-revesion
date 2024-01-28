4a8ae948fa880238c59c0d71c011eeb56a4152ec
mdshakil04 <m.shakil04@yahoo.com>
2024-01-28 23:01:29 +0600

    For-loop

--- while.js
+++ while.js
@@ -1,5 +1,8 @@
  var student = 0;
  while(student < 10){
     console.log("Welcome my dear student")
+    // student = student + 1;
+    // student += 1;
+    console.log(student)
     student++
  }
