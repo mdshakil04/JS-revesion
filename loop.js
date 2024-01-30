a269b0833aa776cc128363c40a3bf51583ecb68d
mdshakil04 <m.shakil04@yahoo.com>
2024-01-30 19:58:51 +0600

    More Problem solved

--- loop.js
+++ loop.js
@@ -4,5 +4,10 @@ for (var i = 0; i < friends.length; i++){
     // console.log(friend)
 }
 for(var i = 1; i <=39 ; i++){
-    console.log("Ajke amar mon bhalo nei" +" "+ i)
+    // console.log("Ajke amar mon bhalo nei" +" "+ i)
+}
+
+// ------Problem 2-------------
+for(var i = 58; i <= 98; i++){
+    console.log(i)
 }
