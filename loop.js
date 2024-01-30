d0723b77828d0cc20e14d8b4fe0b2f55d9bda716
mdshakil04 <m.shakil04@yahoo.com>
2024-01-30 19:52:22 +0600

    Another loop problem solved

--- loop.js
+++ loop.js
@@ -1,5 +1,8 @@
 var friends = ["Shofiq", "Mizan", "Zahid", "Anowar", "Sobuj"]
 for (var i = 0; i < friends.length; i++){
     var friend = friends[i];
-    console.log(friend)
+    // console.log(friend)
+}
+for(var i = 1; i <=39 ; i++){
+    console.log("Ajke amar mon bhalo nei" +" "+ i)
 }
