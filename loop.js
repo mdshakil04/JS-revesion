38239bbd6b26de70efcecc88392fb375033ae6cb
mdshakil04 <m.shakil04@yahoo.com>
2024-01-31 23:47:42 +0600

    Function started

--- loop.js
+++ loop.js
@@ -9,5 +9,10 @@ for(var i = 1; i <=39 ; i++){
 
 // ------Problem 2-------------
 for(var i = 58; i <= 98; i++){
-    console.log(i)
+    // console.log(i)
 }
+function sum(x, y){
+   return x + y;
+}
+var addition = sum(12, 14)
+console.log(addition)
