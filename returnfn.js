9e2cdc7eba9eb28c955508b3141a95a7810f1fbc
mdshakil04 <m.shakil04@yahoo.com>
2024-02-01 23:55:00 +0600

    Practicle fees calculator

--- returnfn.js
+++ returnfn.js
@@ -1,7 +1,16 @@
 function add(num1, num2){
     var sum = num1 + num2
-    console.log(sum)
+    // console.log(sum)
     return sum;
 }
-var total = add(12, 18)
-console.log(total)
+var total = add(120, 180)
+// console.log(total)
+
+function practicleFees(chemistry, physics){
+    var totalFees = chemistry + physics
+    var iGet = (totalFees * 90) / 100;
+    var myTotalFee = iGet * 21
+    return myTotalFee;
+}
+var myTaka = practicleFees(300, 300)
+console.log(myTaka)
