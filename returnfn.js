191322c7ff111a6ae82c15e51b1a13f0ca36989e
mdshakil04 <m.shakil04@yahoo.com>
2024-02-02 00:12:55 +0600

    Bazar Calculator Function

--- returnfn.js
+++ returnfn.js
@@ -5,12 +5,25 @@ function add(num1, num2){
 }
 var total = add(120, 180)
 // console.log(total)
-
-function practicleFees(chemistry, physics){
+// --------Practical Fees calculator---------
+function practicalFees(chemistry, physics){
     var totalFees = chemistry + physics
-    var iGet = (totalFees * 90) / 100;
+    var iGet = (totalFees * 50) / 100;
     var myTotalFee = iGet * 21
     return myTotalFee;
 }
-var myTaka = practicleFees(300, 300)
-console.log(myTaka)
+var myTaka = practicalFees(500, 500)
+// console.log("Alhamdulillah I got " + myTaka + " Taka")
+// --------------------------------------------
+function mySavings(salary, tution){
+    var totalEarnings = salary + tution;
+    var houseRent = 4000;
+    var netElec = 1000;
+    var schoolFees = 900;
+    var bazarKhoros = 10000;
+    var totalCost = houseRent + netElec + schoolFees + bazarKhoros;
+    var remainingMoney = totalEarnings - totalCost;
+    return remainingMoney
+}
+var totalSavings = mySavings(15000, 9000)
+console.log(totalSavings)
