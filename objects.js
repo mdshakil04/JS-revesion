c0746038e9b6c2ccd7c6af05bf3fdbd5f70b916b
mdshakil04 <m.shakil04@yahoo.com>
2024-02-02 11:11:27 +0600

    JavaScript object

--- objects.js
+++ objects.js
@@ -3,3 +3,11 @@ var mobile = {
     price : 19000,
     storage : "64gb"
 }
+var priceCount1 = mobile['price']
+console.log(priceCount1)
+var priceCount2 = mobile.price;
+console.log(priceCount2)
+var propertiesName = Object.keys(mobile);
+console.log(propertiesName)
+var propertiesValues = Object.values(mobile)
+console.log(propertiesValues)
