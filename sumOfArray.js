bd80ddf0b0ba6d7c78bf4b62991169ac26ed20cb
mdshakil04 <m.shakil04@yahoo.com>
2024-02-04 00:30:08 +0600

    Some code added

--- sumOfArray.js
+++ sumOfArray.js
@@ -11,14 +11,18 @@ function sumOfArray(numbers){
 
 
 function getOddNumbers(numbers){
+    const oddNumbers = []
     for(i = 0; i < numbers.length; i++){
         const index = i;
         const element = numbers[index]
-        if (element % 2 === 0){
-            console.log(element)
+        if (element % 2 !== 0){
+            // console.log(element)
+            oddNumbers.push(element)
         }
     }
+    return oddNumbers;
 }
 const myNumbers = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-getOddNumbers(myNumbers)
+const oddNumbers = getOddNumbers(myNumbers)
+console.log(oddNumbers)
 sumOfArray(myNumbers);
