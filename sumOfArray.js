02d6e83dcf19d0d80130db06e1188fdbe541c85d
mdshakil04 <m.shakil04@yahoo.com>
2024-02-04 00:24:41 +0600

    get odd numbers

--- sumOfArray.js
+++ sumOfArray.js
@@ -4,9 +4,21 @@ function sumOfArray(numbers){
         // const index = i;
         const element = numbers[i];
         sum = sum + element;
-        console.log(element, sum)
+        // console.log(element, sum)
     }
     return sum;
 }
+
+
+function getOddNumbers(numbers){
+    for(i = 0; i < numbers.length; i++){
+        const index = i;
+        const element = numbers[index]
+        if (element % 2 === 0){
+            console.log(element)
+        }
+    }
+}
 const myNumbers = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+getOddNumbers(myNumbers)
 sumOfArray(myNumbers);
