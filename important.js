b38870cf9941300f1b4f93a302fcef208395aadc
mdshakil04 <m.shakil04@yahoo.com>
2024-02-03 18:55:11 +0600

    Miles to KM function

---
+++ important.js
@@ -0,0 +1,21 @@
+/**
+  VARIABLE
+ * 1. What is Javascript?
+ * 2. How does javascript works?
+ * 3. What is variable?
+ * 4. Declare a variable
+ * 5. Types of variable? How can find out type a variable
+ * 6. Primitive an d non-primitive data types
+ * 7. Naming convention of JS variables
+ * 8. Math operation
+ * 9. Short hand: +=, -+, *=, /=
+ * 10. ++, --
+ * 11. parseInt, ParseFloat
+ * 12. toFixed
+ */
+
+/**
+ * Array
+ * What is the purpose?
+ * 
+ */
---
+++ milesToKm.js
@@ -0,0 +1,6 @@
+function milesToKm(miles){
+    const kilometer = miles * 1.609;
+    return kilometer;
+}
+const km = milesToKm(15)
+console.log(km)
