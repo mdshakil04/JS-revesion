9f70f48aa76737d2434ab9ed1fcf44d9ad5db2f5
mdshakil04 <m.shakil04@yahoo.com>
2024-02-04 20:31:24 +0600

    JavaScript String

---
+++ case.js
@@ -0,0 +1,8 @@
+const userName = "Shakil04";
+const userInput = "shakil04";
+if(userName.toLowerCase() === userInput.toLowerCase()){
+    console.log("Valid User")
+}
+else{
+    console.log("Invalid User")
+}
