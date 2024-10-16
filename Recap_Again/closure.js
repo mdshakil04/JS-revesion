9feda2360bc7e71b3422efc7c9c575bcf56064ab
mdshakil04 <m.shakil04@yahoo.com>
2024-10-16 23:27:51 +0600

    Closure end for today

--- Recap_Again/closure.js
+++ Recap_Again/closure.js
@@ -1,7 +1,24 @@
-let num1 = 10;
-let num2 = 15;
+// let num1 = 10;
+// let num2 = 15;
 
-function sum(){
-    return num1 + num2;
-}
-console.log(sum())
+// function sum(){
+//     return num1 + num2;
+// }
+// console.dir(sum)
+const sum = () =>{
+    let num = 0;
+    console.log(num , "Console from outer function");
+    return () =>{
+        num += 1;
+        console.log(num, "Console log from inner function");
+    };
+   
+};
+const result = sum();
+result()
+result()
+result()
+result()
+result()
+console.dir(result)
+// console.log(result())
