f27de50563803b560119130ffd573067f58ceffe
mdshakil04 <m.shakil04@yahoo.com>
2024-10-22 21:31:13 +0600

    JS Prototype

--- Recap_Again/prototype.js
+++ Recap_Again/prototype.js
@@ -1,5 +1,11 @@
-var name = "Tomato"
+var name = "Akhi"
 function sayName () {
     console.log("I love",name)
 }
-sayName();
+// sayName();
+function Person(name, age){
+    this.name = name;
+    this.age = age;
+
+}
+const shakil = new Person("Shakil", 32);
