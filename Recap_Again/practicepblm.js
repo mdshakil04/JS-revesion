75ca8d99a84a22654afc6e7f0f8f316215206058
mdshakil04 <m.shakil04@yahoo.com>
2024-10-23 22:40:57 +0600

    Code updated

--- Recap_Again/practicepblm.js
+++ Recap_Again/practicepblm.js
@@ -10,10 +10,11 @@ const orangesPrice = 380;
 const mathematics = 75.25;
 const biology = 65;
 const chemistry = 80;
-const physics = 35.5;
-const bangla = 99;
+const physics = 35.45;
+const bangla = 99.50;
 const totalMarks = mathematics + biology + chemistry + physics + bangla;
-console.log(Math.floor(totalMarks))
-const avaregeMarks = Math.floor(totalMarks / 5);
+// console.log(Math.floor(totalMarks))
+const avaregeMarks = (totalMarks / 5).toFixed(2);
+// const finalAvvrg = avaregeMarks.toFixed(2)
 // const intMarks = Math.floor(averrageMarks)
 console.log(avaregeMarks)
