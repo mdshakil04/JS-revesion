07528c1590fe465a62ad38f83525c7844ca4c52f
mdshakil04 <m.shakil04@yahoo.com>
2024-10-23 22:35:55 +0600

    Problem Solving

---
+++ Recap_Again/practicepblm.js
@@ -0,0 +1,19 @@
+// Problem no. 1
+const myTaka = 1000;
+const applePrice = 350;
+const orangesPrice = 380;
+ const totalCost = applePrice + orangesPrice;
+ const remainingMoney = myTaka - totalCost;
+//  console.log(remainingMoney)
+
+// Problem no. 2
+const mathematics = 75.25;
+const biology = 65;
+const chemistry = 80;
+const physics = 35.5;
+const bangla = 99;
+const totalMarks = mathematics + biology + chemistry + physics + bangla;
+console.log(Math.floor(totalMarks))
+const avaregeMarks = Math.floor(totalMarks / 5);
+// const intMarks = Math.floor(averrageMarks)
+console.log(avaregeMarks)
--- Recap_Again/shorthand.js
+++ Recap_Again/shorthand.js
@@ -1,1 +1,18 @@
-console.log("Hello World")
+let price = "12.6";
+let price1 = parseFloat(price)
+
+// console.log(price)
+// price += 100;
+// console.log(price);
+let fridge = "49.5";
+// fridge -= 5000;
+// console.log(fridge)
+let num1 = parseInt(fridge)
+let num = Number(fridge);
+// console.log(price1)
+// console.log(typeof(price))
+// console.log(typeof(price1))
+// console.log(typeof(num))
+// console.log(typeof(fridge))
+let sum = price1 + num1;
+console.log(sum)
