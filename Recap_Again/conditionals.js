2186f4876813a1eef6df7dbf0c0e653d47dc8f48
mdshakil04 <m.shakil04@yahoo.com>
2024-10-24 20:00:50 +0600

    Conditionals started

---
+++ Recap_Again/conditionals.js
@@ -0,0 +1,12 @@
+// console.log(5 >= 6)
+// console.log(5 === '5')
+const myTaka = 180000;
+const xtremePrice = 171000;
+const hondaSP125 = 150000;
+if(myTaka > xtremePrice){
+    console.log("Insha-Allah! I will buy Hero Xtreme 125R")
+}else if(myTaka > hondaSP125 ){
+    console.log("I will buy Honda SP 125")
+}else{
+    console.log("I am happy with my present bike")
+}
